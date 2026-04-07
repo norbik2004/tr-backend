ced3ad7c747537e11008b686b2bab96932db43b8
Norbert <kieler103@gmail.com>
2026-04-07 12:07:40 +0200

    feat: #dev migration

---
+++ tr-repository/Migrations/20260407100658_fix-entities.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace tr_repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixentities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "UserSettings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserSettings",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserSettings",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Posts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PostPlatforms",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "PostPlatforms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "PostPlatforms",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Platforms",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Platforms",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSettings_UserId",
+                table: "UserSettings",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserSettings_UserId",
+                table: "UserSettings");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserSettings");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserSettings");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserSettings");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PostPlatforms");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "PostPlatforms");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "PostPlatforms");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Platforms");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Platforms");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings",
+                column: "UserId");
+        }
+    }
+}
